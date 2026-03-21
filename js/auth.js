634bdc99bfcc596d5f836dcf2efee8ee0008cb71
vendelxz <jwend3l@gmail.com>
2026-03-21 00:16:10 -0300

    feat: página de login implementada

---
+++ js/auth.js
@@ -0,0 +1,37 @@
+
+const formLogin = document.getElementById('form-login');
+
+formLogin.addEventListener('submit', async (evento) => {
+    
+    evento.preventDefault(); 
+
+    
+    const email = document.getElementById('email').value;
+    const senha = document.getElementById('senha').value;
+
+    try {
+       
+        const resposta = await fetch('http://localhost:8080/api/auth/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email: email, senha: senha })
+        });
+
+        if (resposta.ok) {
+            
+            const dados = await resposta.json();
+            
+            //Guardar o token para navegar nas requisições da API posteriormente...
+            localStorage.setItem('token', dados.token);
+            window.location.href = 'index.html'; 
+        } else {
+            // Se devolver 403, 404, etc.
+            alert('Credenciais inválidas. Por favor, tente novamente.');
+        }
+    } catch (erro) {
+        console.error('Erro de ligação:', erro);
+        alert('Não foi possível contactar o servidor. Verifique se a API está a correr.');
+    }
+});
